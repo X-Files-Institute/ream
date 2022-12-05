c6fe5f6b8dee241f5908830e24bc7f77f2f3b2ad
Muqiu Han <muqiu-han@outlook.com>
2022-12-05 21:51:55 +0800

    Update project info

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/rackever.scrbl" ())))
-(define pkg-desc "minimalist, fast Racket web server")
+(define pkg-desc "low-level, and zero dependencies Racket web server")
 (define version "0.0.1")
 (define pkg-authors '(MuqiuHan))
 (define license '(MIT))
